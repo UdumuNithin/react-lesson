ef9d6112d1ff35db34e214bbe3923088909c9150
UdumuNithin <unithin58@gmail.com>
2025-05-25 19:40:31 +0530

    update

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,11 +1,21 @@
-.Box-Container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 20px auto;
-  padding: 20px;
-  max-width: 400px;
-  border: 2px dashed #60a5fa;
-  border-radius: 12px;
-  background-color: #f0f9ff;
+import React from "react";
+import Box1 from "./Box1";
+import Box2 from "./Box2";
+import "./Box.css"; 
+
+export default function Box(props) {
+  //Method 1 using if statement
+  //   if (props.flag === 1) {
+  //     return <Box1 />;
+  //   } else {
+  //     return <Box2 />;
+  //   }
+  //Method 2 using ternary operator
+  {
+    return props.flag === 1 ? <Box1 /> : <Box2 />;
+  }
+
+  ///Method 3
+  //   if (props.flag === 1) return <Box1 />;
+  //   else return <Box2 />;
 }
